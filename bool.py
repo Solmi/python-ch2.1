68e709ae23453b4684ba570f106ddedef8c1f932
Solmi <schoi41@hawk.iit.edu>
2018-12-10 16:15:24 +0900

    1st commit

---
+++ bool.py
@@ -0,0 +1,17 @@
+# bool(부울) 참이나 거짓을 나타내는
+# True, False 값을 가진다
+
+a = 1
+print(a > 10)
+print(a < 10)
+
+b = a == 1
+print(b, type(b))
+print(b*10)
+
+a = False
+print(a*10)
+
+print(True * False)
+
+print(a)
